6b5295864b46d7334413da634c8b68a2958a5636
alexjo <alexjo@blinkbox.com>
2014-09-03 16:45:53 +0100

    missing update

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -2,7 +2,7 @@ Given(/a valid user account(?: ready for use on a new device)?$/) do
   user_interface.deregister_all_devices
 end
 Given(/^the anonymous library screen is displayed$/) do
-  enter_app_as_anonymous_user()
+  bypass_welcome_screen
   expect_page(anonymous_library_page)
 end
 Then(/^the ([A-Za-z\-\s]*) should be displayed$/) do |page_name|
